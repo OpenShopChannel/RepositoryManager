4a93db5d88f5f219d94f5c722c12c8a58da1bea9
dhtdht020 <dhtdht020@gmail.com>
2023-07-30 00:07:24 +0300

    Added check for empty long description

--- hbb/routes.py
+++ hbb/routes.py
@@ -83,7 +83,7 @@ def apps_list():
         # Short Description
         content.add_line(app["metaxml"]["app"]["short_description"])
         # Long Description
-        if "long_description" in app["metaxml"]["app"]:
+        if ("long_description" in app["metaxml"]["app"]) and (app["metaxml"]["app"]["long_description"] is not None):
             if len(app["metaxml"]["app"]["long_description"]) > 128:
                 content.add_line(app["metaxml"]["app"]["long_description"].replace('\n', ' ')[:128] + "...")
             else:
