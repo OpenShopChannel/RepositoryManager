cdb933dd6be689436799dc1113b791ed1047889a
Artuto <artuto@artuto.xyz>
2024-02-11 19:51:48 -0600

    Implement template rendering

--- src/main/java/org/oscwii/repositorymanager/DanboApp.java
+++ src/main/java/org/oscwii/repositorymanager/DanboApp.java
@@ -2,22 +2,22 @@ package org.oscwii.repositorymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DanboApp
 {
+    @GetMapping("/")
+    public String hello(Model model)
+    {
+        return "hello_world";
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(DanboApp.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
-    {
-        return String.format("Hello %s!", name);
     }
 }
---
+++ src/main/java/org/oscwii/repositorymanager/config/WebConfig.java
@@ -0,0 +1,48 @@
+package org.oscwii.repositorymanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer
+{
+    @Bean
+    public SpringResourceTemplateResolver templateResolver()
+    {
+        var templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine()
+    {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver()
+    {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
