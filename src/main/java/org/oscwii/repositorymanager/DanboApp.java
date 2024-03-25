d13b0602c785835fefc569a10f4363cc3d744749
Artuto <artuto@artuto.xyz>
2024-03-25 13:09:44 -0600

    Added debug endpoint to trigger index

--- src/main/java/org/oscwii/repositorymanager/DanboApp.java
+++ src/main/java/org/oscwii/repositorymanager/DanboApp.java
@@ -1,12 +1,14 @@
 package org.oscwii.repositorymanager;
 
 import org.jdbi.v3.spring5.EnableJdbiRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
@@ -14,10 +16,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @ConfigurationPropertiesScan(value = "org.oscwii.repositorymanager.config.repoman")
 public class DanboApp
 {
+    @Autowired
+    private RepositoryIndex index;
+
     @GetMapping("/")
     public String hello(Model model)
     {
         return "hello_world";
+    }
+
+    // TODO remove debug
+    @GetMapping("/debug")
+    @ResponseBody
+    public String debug()
+    {
+        index.update(true);
+        return "running";
     }
 
     public static void main(String[] args)
